daecdc4bf241f990d41117ba55cf837032d22a50
Chris Palmer <chris@noncombatant.org>
2021-07-05 14:46:30 -0700

    Move stuff into src/{filter,records}.rs.

---
+++ src/records.rs
@@ -0,0 +1,55 @@
+use getopt::Opt;
+use std::io::{stdout, Write};
+use std::process::exit;
+
+use crate::sub_slicer::SubSlicer;
+use crate::util::map_file;
+use crate::{DEFAULT_INPUT_DELIMITER, DEFAULT_OUTPUT_DELIMITER};
+
+pub fn records_help() {
+    eprintln!("TODO: records_help");
+    exit(1);
+}
+
+pub fn records_main(arguments: &[String]) {
+    let mut options = getopt::Parser::new(&arguments, "d:ho:");
+
+    let mut input_delimiter = String::from(DEFAULT_INPUT_DELIMITER);
+    let mut output_delimiter = String::from(DEFAULT_OUTPUT_DELIMITER);
+    loop {
+        match options.next().transpose().unwrap() {
+            None => break,
+            Some(opt) => match opt {
+                Opt('d', Some(string)) => input_delimiter = string.clone(),
+                Opt('h', None) => records_help(),
+                Opt('o', Some(string)) => output_delimiter = string.clone(),
+                _ => records_help(),
+            },
+        }
+    }
+
+    // TODO: Support this someday.
+    //let input_delimiter = Regex::new(&input_delimiter).unwrap();
+    let input_delimiter_bytes = input_delimiter.as_bytes();
+    let output_delimiter_bytes = output_delimiter.as_bytes();
+
+    let (_, arguments) = arguments.split_at(options.index());
+
+    if arguments.is_empty() {
+        // TODO: read stdin
+    } else {
+        for pathname in arguments {
+            if let Some(mapped) = map_file(&pathname) {
+                let slicer = SubSlicer {
+                    slice: &mapped,
+                    input_delimiter: &input_delimiter_bytes,
+                    start: 0,
+                };
+                for s in slicer {
+                    stdout().write_all(s).unwrap();
+                    stdout().write_all(output_delimiter_bytes).unwrap();
+                }
+            }
+        }
+    }
+}
