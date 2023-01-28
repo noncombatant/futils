90b0324701af9a74c264b75f0fd8f7f3564de0f0
Chris Palmer <chris@noncombatant.org>
2023-01-28 12:45:13 -0800

    Add -v option
    Also clean up some `unwrap`s, and add a TODO.

--- src/apply.rs
+++ src/apply.rs
@@ -12,34 +12,37 @@ pub fn apply_help() {
 }
 
 pub fn apply_main(arguments: &[String]) -> ShellResult {
-    let mut options = getopt::Parser::new(&arguments, "d:ho:x:");
+    let mut options = getopt::Parser::new(&arguments, "d:ho:x:v");
 
     let mut input_delimiter = String::from(DEFAULT_INPUT_DELIMITER);
     let mut output_delimiter = String::from(DEFAULT_OUTPUT_DELIMITER);
     let mut command = String::new();
+    let mut verbose = false;
 
     loop {
-        match options.next().transpose().unwrap() {
+        match options.next().transpose()? {
             None => break,
             Some(opt) => match opt {
                 Opt('d', Some(string)) => input_delimiter = string.clone(),
                 Opt('h', None) => apply_help(),
                 Opt('o', Some(string)) => output_delimiter = string.clone(),
                 Opt('x', Some(string)) => command = string.clone(),
+                Opt('v', None) => verbose = true,
                 _ => apply_help(),
             },
         }
     }
 
     // TODO: Support this someday.
-    //let input_delimiter = Regex::new(&input_delimiter).unwrap();
-    let input_delimiter = unescape_backslashes(&input_delimiter).unwrap();
+    //let input_delimiter = Regex::new(&input_delimiter)?;
+    let input_delimiter = unescape_backslashes(&input_delimiter)?;
     let input_delimiter_bytes = input_delimiter.as_bytes();
-    let output_delimiter = unescape_backslashes(&output_delimiter).unwrap();
+    let output_delimiter = unescape_backslashes(&output_delimiter)?;
     let output_delimiter_bytes = output_delimiter.as_bytes();
 
     let (_, arguments) = arguments.split_at(options.index());
 
+    let mut code = 0;
     if arguments.is_empty() {
         eprintln!("TODO: Reading from stdin not implemented yet. Sorry!");
         apply_help();
@@ -52,11 +55,18 @@ pub fn apply_main(arguments: &[String]) -> ShellResult {
                     start: 0,
                 };
                 for s in slicer {
-                    run_command(&command, s);
-                    stdout().write_all(output_delimiter_bytes).unwrap();
+                    match run_command(&command, s, verbose) {
+                        Ok(c) => if c != 0 && code == 0 {
+                            code = c
+                        },
+                        _ => panic!("We're gonna die"),
+                    };
+                    // TODO: First, remove the trailing \n, if
+                    // output_delimiter_bytes is not \n.
+                    //stdout().write_all(output_delimiter_bytes)?;
                 }
             }
         }
     }
-    Ok(0)
+    Ok(code)
 }
