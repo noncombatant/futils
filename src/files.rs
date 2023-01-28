24995d8a9d17898f02fb20bc64f5eb0d1687007d
Chris Palmer <chris@noncombatant.org>
2023-01-28 13:52:00 -0800

    Simplified implementation of matching functionality
    Shows the way for additional development.

--- src/files.rs
+++ src/files.rs
@@ -1,10 +1,9 @@
 use getopt::Opt;
-use regex::bytes::Regex;
+use regex::Regex;
 use std::io::{stdout, Write};
 use std::process::exit;
 use walkdir::{DirEntry, WalkDir};
 
-use crate::predicate::Predicate;
 use crate::util::{unescape_backslashes, ShellResult};
 use crate::DEFAULT_OUTPUT_DELIMITER;
 
@@ -21,12 +20,11 @@ pub fn files_help() {
 }
 
 pub fn files_main(arguments: &[String]) -> ShellResult {
-    let mut options = getopt::Parser::new(&arguments, "ahm:o:p:x:");
+    let mut options = getopt::Parser::new(&arguments, "ahm:o:");
     let mut show_all = false;
     let mut output_delimiter = String::from(DEFAULT_OUTPUT_DELIMITER);
-    let mut match_expression = String::new();
-    let mut prune_expression = String::new();
-    let mut match_command = String::new();
+
+    let mut match_expressions = Vec::new();
 
     loop {
         match options.next().transpose()? {
@@ -34,36 +32,12 @@ pub fn files_main(arguments: &[String]) -> ShellResult {
             Some(opt) => match opt {
                 Opt('a', None) => show_all = true,
                 Opt('h', None) => files_help(),
-                Opt('m', Some(string)) => match_expression = string.clone(),
+                Opt('m', Some(string)) => match_expressions.push(Regex::new(&string)?),
                 Opt('o', Some(string)) => output_delimiter = string.clone(),
-                Opt('p', Some(string)) => prune_expression = string.clone(),
-                Opt('x', Some(string)) => match_command = string.clone(),
                 _ => files_help(),
             },
         }
     }
-
-    let conditions = [&match_expression, &prune_expression, &match_command];
-    let count = conditions.iter().filter(|i| !i.is_empty()).count();
-    if count != 1 {
-        // TODO: Make it possible to pass more than 1, and AND them all
-        // together.
-        //eprintln!("Use exactly 1 of -m, -p, or -x.");
-        //files_help();
-    }
-
-    let re: Regex;
-    let predicate = if !match_command.is_empty() {
-        Predicate::MatchCommand(&match_command)
-    } else if !match_expression.is_empty() {
-        re = Regex::new(&match_expression)?;
-        Predicate::MatchExpression(&re)
-    } else if !prune_expression.is_empty() {
-        re = Regex::new(&prune_expression)?;
-        Predicate::PruneExpression(&re)
-    } else {
-        Predicate::Nothing
-    };
 
     let output_delimiter = unescape_backslashes(&output_delimiter)?;
     let output_delimiter_bytes = output_delimiter.as_bytes();
@@ -76,9 +50,8 @@ pub fn files_main(arguments: &[String]) -> ShellResult {
     }
     for p in pathnames {
         // TODO: Separate all this out into a function; it's too deeply nested.
-        // TODO: Apply predicate.
         let mut it = WalkDir::new(p).into_iter();
-        loop {
+        'outer: loop {
             let entry = match it.next() {
                 None => break,
                 Some(e) => e,
@@ -96,16 +69,24 @@ pub fn files_main(arguments: &[String]) -> ShellResult {
                 }
                 continue;
             }
+
             let p = entry.path();
-            match p.to_str() {
-                Some(s) => {
-                    stdout().write_all(s.as_bytes())?;
-                    stdout().write_all(output_delimiter_bytes)?;
-                }
+            let pathname = match p.to_str() {
+                Some(s) => s,
                 None => {
                     eprintln!("pathname not valid Unicode: '{}'", p.display());
+                    continue;
+                }
+            };
+
+            for re in &match_expressions {
+                if !re.is_match(pathname) {
+                    continue 'outer;
                 }
             }
+
+            stdout().write_all(pathname.as_bytes())?;
+            stdout().write_all(output_delimiter_bytes)?;
         }
     }
     Ok(0)
