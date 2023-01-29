bfad2f5f24daa0e5a89b00840be209676f5fb2e7
Chris Palmer <chris@noncombatant.org>
2023-01-28 17:58:14 -0800

    Help message for main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,21 +16,42 @@ use files::files_main;
 use filter::filter_main;
 use records::records_main;
 use test::test_main;
-use util::{file_name, ShellResult};
+use util::{file_name, help};
 
 // TODO: Support regex someday.
 //static DEFAULT_INPUT_DELIMITER: &str = r"(\r\n|\n|\r)";
 static DEFAULT_INPUT_DELIMITER: &str = "\n";
 static DEFAULT_OUTPUT_DELIMITER: &str = "\n";
 
-fn futils_help(i: i32) -> ShellResult {
-    eprintln!("TODO: futils_help");
-    if i == 0 {
-        Ok(i)
-    } else {
-        Err(Box::<dyn Error>::from("invalid invocation"))
-    }
-}
+const HELP_MESSAGE: &str = "futils - functional shell utilities
+
+Usage:
+
+futils consists of many sub-commands:
+
+    apply
+    files
+    filter
+    records
+        ... and more to come.
+
+To learn more about each one, run
+
+    futils *sub-command* -h
+
+e.g.
+
+    futils apply -h
+
+If installed correctly, *futils* should also allow you invoke the utilities
+directly, e.g.
+
+    apply -h
+    files -h
+        ... and so on.
+
+NOTE: The attempt to make modern, rational Unix shell utilities is addmitedly
+somewhat futile. French speakers may pronounce it « foutils » if they like.";
 
 fn main() {
     let mut arguments = env::args().collect::<Vec<String>>();
@@ -39,22 +60,26 @@ fn main() {
     let basename = file_name(&arguments[0]).expect(conversion_error_message);
     if basename.eq("futils") {
         if arguments.len() < 2 {
-            futils_help(-1).expect("NOTREACHED");
-            exit(-1);
+            help(-1, HELP_MESSAGE);
         }
         arguments.remove(0);
     }
 
     let basename = file_name(&arguments[0]).expect(conversion_error_message);
+    if basename == "help" {
+        help(0, HELP_MESSAGE);
+    }
     if let Err(e) = match basename {
         "apply" => apply_main(&arguments),
         "files" => files_main(&arguments),
         "filter" => filter_main(&arguments),
         "records" => records_main(&arguments),
 
-        "help" => futils_help(0),
         "test" => test_main(&arguments),
-        _ => futils_help(-1),
+        _ => {
+            help(-1, HELP_MESSAGE);
+            Err(Box::<dyn Error>::from("NOTREACHED"))
+        }
     } {
         eprintln!("{}", e);
         exit(-1)
