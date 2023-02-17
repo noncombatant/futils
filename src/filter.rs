3ba237477657fd5ed279189cedb7441444e6d46a
Chris Palmer <chris@noncombatant.org>
2023-02-16 17:54:24 -0800

    Support multiple filters of multiple types

--- src/filter.rs
+++ src/filter.rs
@@ -2,9 +2,8 @@ use getopt::Opt;
 use regex::bytes::Regex;
 use std::io::{stdout, Write};
 
-use crate::predicate::Predicate;
 use crate::sub_slicer::SubSlicer;
-use crate::util::{help, map_file, unescape_backslashes, ShellResult};
+use crate::util::{help, map_file, run_command, unescape_backslashes, ShellResult};
 use crate::{DEFAULT_INPUT_DELIMITER, DEFAULT_OUTPUT_DELIMITER};
 
 const HELP_MESSAGE: &str = "filter - filter records from files by patterns
@@ -21,8 +20,7 @@ Searches the given *file*(s) for records that match the given specifications:
         expression.
     -x  Print records for which the given *command* exited with status 0.
 
-BUG: Currently, you must, and can only, supply, exactly 1 of -m, -p, or -x. I’ll
-fix this soon.
+TODO update docs to mirror files.rs
 
 Regular expressions use the Rust regex library syntax
 (https://docs.rs/regex/latest/regex/).
@@ -41,9 +39,10 @@ pub fn filter_main(arguments: &[String]) -> ShellResult {
 
     let mut input_delimiter = String::from(DEFAULT_INPUT_DELIMITER);
     let mut output_delimiter = String::from(DEFAULT_OUTPUT_DELIMITER);
-    let mut match_expression = String::new();
-    let mut prune_expression = String::new();
-    let mut match_command = String::new();
+    let mut match_expressions = Vec::new();
+    let mut prune_expressions = Vec::new();
+    let mut match_commands = Vec::new();
+    let mut verbose = false;
 
     loop {
         match options.next().transpose()? {
@@ -51,36 +50,15 @@ pub fn filter_main(arguments: &[String]) -> ShellResult {
             Some(opt) => match opt {
                 Opt('d', Some(string)) => input_delimiter = string.clone(),
                 Opt('h', None) => help(0, HELP_MESSAGE),
-                Opt('m', Some(string)) => match_expression = string.clone(),
+                Opt('m', Some(string)) => match_expressions.push(Regex::new(&string)?),
                 Opt('o', Some(string)) => output_delimiter = string.clone(),
-                Opt('p', Some(string)) => prune_expression = string.clone(),
-                Opt('x', Some(string)) => match_command = string.clone(),
+                Opt('p', Some(string)) => prune_expressions.push(Regex::new(&string)?),
+                Opt('x', Some(string)) => match_commands.push(string.clone()),
+                Opt('v', None) => verbose = true,
                 _ => help(-1, HELP_MESSAGE),
             },
         }
     }
-
-    let conditions = [&match_expression, &prune_expression, &match_command];
-    let count = conditions.iter().filter(|i| !i.is_empty()).count();
-    if count != 1 {
-        // TODO: Make it possible to pass more than 1, and AND them all
-        // together.
-        eprintln!("Use exactly 1 of -m, -p, or -x.");
-        help(-1, HELP_MESSAGE);
-    }
-
-    let re: Regex;
-    let predicate = if !match_command.is_empty() {
-        Predicate::MatchCommand(&match_command)
-    } else if !match_expression.is_empty() {
-        re = Regex::new(&match_expression)?;
-        Predicate::MatchExpression(&re)
-    } else if !prune_expression.is_empty() {
-        re = Regex::new(&prune_expression)?;
-        Predicate::PruneExpression(&re)
-    } else {
-        Predicate::Nothing
-    };
 
     let input_delimiter = unescape_backslashes(&input_delimiter)?;
     let input_delimiter_bytes = input_delimiter.as_bytes();
@@ -95,6 +73,7 @@ pub fn filter_main(arguments: &[String]) -> ShellResult {
         help(-1, HELP_MESSAGE);
     } else {
         for pathname in arguments {
+            // TODO: Separate all this out into a function; it's too deeply nested.
             match map_file(pathname) {
                 Ok(mapped) => {
                     let slicer = SubSlicer {
@@ -102,11 +81,39 @@ pub fn filter_main(arguments: &[String]) -> ShellResult {
                         input_delimiter: input_delimiter_bytes,
                         start: 0,
                     };
-                    for s in slicer {
-                        if predicate.evaluate(s) {
-                            stdout().write_all(s)?;
-                            stdout().write_all(output_delimiter_bytes)?;
+
+                    'outer: for s in slicer {
+                        for re in &prune_expressions {
+                            if re.is_match(s) {
+                                continue 'outer;
+                            }
                         }
+                        for re in &match_expressions {
+                            if !re.is_match(s) {
+                                continue 'outer;
+                            }
+                        }
+                        for command in &match_commands {
+                            match run_command(command, s, verbose) {
+                                Ok(status) => {
+                                    if status != 0 {
+                                        continue 'outer;
+                                    }
+                                }
+                                Err(e) => {
+                                    eprintln!(
+                                        "{} \"{}\": {}",
+                                        command,
+                                        String::from_utf8_lossy(s),
+                                        e
+                                    );
+                                    continue 'outer;
+                                }
+                            }
+                        }
+
+                        stdout().write_all(s)?;
+                        stdout().write_all(output_delimiter_bytes)?;
                     }
                 }
                 Err(e) => {
