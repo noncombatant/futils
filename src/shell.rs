f151d992d2bddd1ded58f04982ddf707d2212cce
Chris Palmer <chris@noncombatant.org>
2023-04-16 18:22:21 -0700

    Make `-P` `bool`

--- src/shell.rs
+++ src/shell.rs
@@ -124,7 +124,7 @@ pub(crate) struct Options {
     pub(crate) enumerate: bool,
 
     /// `-P`
-    pub(crate) parallel: u16,
+    pub(crate) parallel: bool,
 
     /// `-p`
     pub(crate) prune_expressions: Vec<Regex>,
@@ -183,7 +183,7 @@ impl Options {
             mtime_expressions: Vec::new(),
             match_expressions: Vec::new(),
             enumerate: false,
-            parallel: 1,
+            parallel: false,
             prune_expressions: Vec::new(),
             output_record_delimiter: Vec::from(DEFAULT_OUTPUT_RECORD_DELIMITER),
             input_record_delimiter: Regex::new(DEFAULT_INPUT_RECORD_DELIMITER)?,
@@ -222,7 +222,7 @@ pub(crate) fn parse_options(arguments: &[String]) -> Result<(Options, &[String])
                 Opt('M', Some(s)) => options.mtime_expressions.push(Time::new(&s)?),
                 Opt('m', Some(s)) => options.match_expressions.push(Regex::new(&s)?),
                 Opt('n', None) => options.enumerate = true,
-                Opt('P', Some(s)) => options.parallel = str::parse::<u16>(&s)?,
+                Opt('P', None) => options.parallel = true,
                 Opt('p', Some(s)) => options.prune_expressions.push(Regex::new(&s)?),
                 Opt('R', Some(s)) => {
                     options.output_record_delimiter =
